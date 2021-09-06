f641292a6314b731b91da7e0a03bbe0db33bd73f
Grace Pan <gracepan12@gmail.com>
2021-09-07 01:55:20 +0800

    added deployed api for client

--- client/server.js
+++ client/server.js
@@ -9,5 +9,4 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/client/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
