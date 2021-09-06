a685e712dce22a19c407e1c7b141552fb669d554
Grace Pan <gracepan12@gmail.com>
2021-09-07 01:33:34 +0800

    trying deploy

--- client/server.js
+++ client/server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "./dist/client"));
+app.use(express.static(path.join(__dirname, "/dist/client")));
 
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/client/" });
+  res.sendFile(path.join(__dirname + "/dist/client/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
